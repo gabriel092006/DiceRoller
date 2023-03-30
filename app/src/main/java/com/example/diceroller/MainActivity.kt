0f6b0e31e8e63ccd7766e228e9f4f661dcc92e53
gabriel092006 <gabriel.vicente32@etec.sp.gov.br>
2023-03-30 08:18:33 -0300

    DiceRoller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.example.diceroller.R.id.result_text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +16,13 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener { rollDice() }
     }
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-             Toast.LENGTH_SHORT).show()
+       // Toast.makeText(this, "button clicked",
+        //  Toast.LENGTH_SHORT).show()
+             val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        resultText.text = randomInt.toString()
 
     }
     }
